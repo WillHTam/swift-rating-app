0d0e21b8bd53086f96d9e804a806515c9136869c
William T <william.tam18@gmail.com>
2017-06-21 22:37:57 +0800

    Initial Commit

---
+++ CatRater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatRater
+//
+//  Created by William on 21/6/17.
+//  Copyright © 2017 William T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
