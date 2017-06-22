8cdb7a51e454eb13a277ee4b4dc7b89b254e8d88
William T <william.tam18@gmail.com>
2017-06-22 22:35:23 +0800

    Can change title, bring up keyboard, choose picture

--- CatRater/ViewController.swift
+++ CatRater/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 // By adding UITextFieldDelegate, compiler knows that ViewController class can be a text field delegate
     
@@ -17,8 +17,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var catNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     // IBOutlet tells XCode to connect nameTextField to the Interface Builder
     // Weak allows this to be deallocated. The strong reference is held by the object's superview, so if the superview exists so does the object
+    // Image (3rd) has no interaction component, if ctrl-dragged 'Action' is not available. Thus a gesture recognizer must be created.
+    // Gesture recognizers are objects you attach to a view that respond to input like a control. Gesture recognizers interpret touches and determine whether they correspond to an action like a swipe pinch or rotation
+    // Added a 'Tap Gesture Recognizer' from the object library
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,8 +59,46 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // an example of target-action in iOS design
     // event is user tapping Set Default Text Button
     // action is setDefaultLabelText(_)
-    // The taarget is ViewController
+    // The target is ViewController
     // The sender is the 'Set Default Label Text' button
-
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard if image is tapped
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerContorller is a view controller that lets a user pick media from their photo library
+        let imagePickerController = UIImagePickerController()
+        
+        // Allow photos to be picked only from Photo Library
+        imagePickerController.sourceType = .photoLibrary
+        // .photoLibrary is an enumeration, which means that its type is already known and thus you don't have to write out the complete UIImagePickerContollerSourceType.photolibrary
+        
+        // Notify ViewController when the user picks an image
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    // Dragged from the 'Gesture Recognizer' square in the storyboard scene dock
+    
+    //MARK: UIImagePickerControllerDelegate
+    //Called on user cancelling image select
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user cancels
+        dismiss(animated: true, completion: nil)
+    }
+    // Called when user selects photo, in this case display in image view
+    // To use this, must have permission to access the user's photo library, do so from the Info.plist. Add 'Privacy - Photo Library Usage Description', Type: String, and add the reason of why you need this permission in Value
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        // The info dictionary always contins the orignal image that was slected in the picker. This code access the original, unedited image from the info dictionary, unwraps the optional and casts it as an UIImage object.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+    
+        // Set photoImageView to display the selected image
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
 }
 
