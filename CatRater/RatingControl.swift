f4e23c5d37bc175046a99023dee4f2bfd095fe2c
William T <william.tam18@gmail.com>
2017-06-23 01:21:53 +0800

    Multiple buttons created with for-in loop, and they respond to touch

--- CatRater/RatingControl.swift
+++ CatRater/RatingControl.swift
@@ -12,6 +12,11 @@ import UIKit
 // init(frame:) for programatically
 // init(coder:) for storyboard
 class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
 
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -24,23 +29,44 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton){
+        print("Button Pressed 👍")
+    }
+    
     //MARK: Private Methods
     // Private methods can only be code within the declaring class, which ensures that they are not unexpectedly or accidentally called from outside
     private func setupButtons() {
         
-        // Create the buton
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
+        // Create 5 buttons with this for-in loop with half-open range operator (..<)
+        // Underscore is a wildcard when you dont need to know which iteration of the loop is currently executing
+        // Auto indent with Ctrl+I
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
         
-        // Button's constraints, together defines it as a fixed 44x44pt. object
-        // Removes automatically generated constraints. Typically when using Auto Layout you want to replace these constraints with your own
-        button.translatesAutoresizingMaskIntoConstraints = false
-        // Create constraints here
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            // Button's constraints, together defines it as a fixed 44x44pt. object
+            // Removes automatically generated constraints. Typically when using Auto Layout you want to replace these constraints with your own
+            button.translatesAutoresizingMaskIntoConstraints = false
+            // Create constraints here
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
         
-        // Add the button to the stack
-        addArrangedSubview(button)
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            // addTarget attaches the 'ratingButtonTapped()' method to the button object which gets triggered on tap
+            // The target is 'self' which is the 'RatingControl' object
+            // #selector returns the Selector value for the provided method, and lets the system call the action method 'ratingButtonTapped'. addTarget is a method that requires selectors
+            // UIControlEvents.touchUpInside (used as an enumeration) responds to when the user touches the button and then lifts up their finger. Better than '.touchDown' because you can cancel by dragging your finger outside the button
+            // func doSomething(sender: UIButton, forEvent event: UIEvent)
+        
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
     
     /*
